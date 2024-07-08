4cbe1cc15c2d11f4d462a9932c327d0924c41090
Giovanni-Val <Gio95.vit@gmai.com>
2024-07-08 22:14:40 +0200

    Esercizio use effect

---
+++ Es React/Use Effect/Clock/src/App.jsx
@@ -0,0 +1,12 @@
+import { Clock } from "./Components/Clock";
+
+export function App() {
+  return (
+    <div>
+      <h1>Behold the mighty clock!</h1>
+      <Clock></Clock>
+    </div>
+  )
+}
+
+export default App;
