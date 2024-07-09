187cbeac0b25f2c1c82b9da4c9d65c2c25bd7b50
Giovanni-Val <Gio95.vit@gmai.com>
2024-07-09 16:15:08 +0200

    Esercizio Event Handling

---
+++ Es React/Event handling/Handling Events/src/App.jsx
@@ -0,0 +1,11 @@
+import { MouseClicker } from "./components/MouseClick";
+
+export function App() {
+  return (
+    <div>
+      <MouseClicker></MouseClicker>
+    </div>
+  )
+}
+
+export default App
