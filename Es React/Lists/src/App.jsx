9bbd798efc3ee9fb9e0082390f1ac43e74b0b315
Giovanni-Val <Gio95.vit@gmai.com>
2024-07-16 19:13:19 +0200

    Color List

--- Es React/Lists/src/App.jsx
+++ Es React/Lists/src/App.jsx
@@ -1,11 +1,22 @@
-import { List } from "./components/List"
+import Color from "./components/Color"
+import Colors from "./components/Colors"
+
 
 export function App() {
+
+  const colorsArray = [
+    { id: 1, name: 'Red' },
+    { id: 2, name: 'Green' },
+    { id: 3, name: 'Blue' },
+    { id: 4, name: 'Yellow' },
+    { id: 5, name: 'Purple' }
+  ];
 
   return (
 
     <div>
-      <List names={["Jane", "Billy", "John", "Kate"]}></List>
+      <Color color={colorsArray}></Color>
+      <Colors></Colors>
     </div>
   )
 }
